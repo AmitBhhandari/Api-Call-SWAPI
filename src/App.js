de32e4d3904edb09620ec9008d1f86c06db0f993
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2022-12-15 16:26:04 +0530

    ASYNC AWAIT

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  async function fetchMoviesHandler() {
+   const response= await fetch("https://swapi.dev/api/films")
+      const data= await response.json()
+      
+        const transformedMovies = data.results.map((moviesData) => {
+          return {
+            id: moviesData.id,
+            title: moviesData.title,
+            openingText: moviesData.opening_crawl,
+            releaseDate: moviesData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+     
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
