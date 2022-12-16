6692b1b1ee6d28b985dd0aecd59b40361b6c20e0
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2022-12-16 13:49:21 +0530

    usEffect() for rendering movies on display without button get clicked and optimization usCallback()

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,7 +8,9 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+
+
+  const  fetchMoviesHandler=useCallback ( async ()=> {
     setIsLoading(true);
     setError(null);
     try {
@@ -31,7 +33,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  },[]);
+
+  useEffect(()=>{
+    fetchMoviesHandler()
+  },[fetchMoviesHandler])
   //let content = <p>Found no movies</p>;
 
   //if (movies.length > 0) {
