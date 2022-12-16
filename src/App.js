dc201f656db7dee435a0d3a0ded94657e8056cba
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2022-12-16 21:26:18 +0530

    fetching data adding data and deleting from database

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useCallback, useEffect, useState } from "react";
- 
+import React, { useState, useEffect, useCallback } from "react";
+
 import MoviesList from "./components/MoviesList";
-import MoviesForm from "./components/MoviesForm";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -9,65 +9,80 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-
-
-  const  fetchMoviesHandler=useCallback ( async ()=> {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films");
+      const response = await fetch(
+        "https://react-http-b2074-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!response.ok) {
-        throw new Error("Something went wrong");
+        throw new Error("Something went wrong!");
       }
-      const data = await response.json();
 
-      const transformedMovies = data.results.map((moviesData) => {
-        return {
-          id: moviesData.id,
-          title: moviesData.title,
-          openingText: moviesData.opening_crawl,
-          releaseDate: moviesData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const data = await response.json();
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-  },[]);
+  }, []);
 
-  useEffect(()=>{
-    fetchMoviesHandler()
-  },[fetchMoviesHandler])
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-http-b2074-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  }
+  const onDeleteHandler = () => {
+    fetchMoviesHandler();
+  };
 
-  //let content = <p>Found no movies</p>;
+  let content = <p>Found no movies.</p>;
 
-  //if (movies.length > 0) {
-  //  <MoviesList movies={movies} />;
-  //}
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
 
-  //if (error) {
-  //  content = { error };
-  //}
+  if (error) {
+    content = <p>{error}</p>;
+  }
 
-  //if (isLoading) {
-    //<p>Loading..</p>;
-  //}
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
-      <MoviesForm/>
+      <section>
+        <AddMovie onAddMovie={onDeleteHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error &&<p>Found No Movies</p>}
-        {!isLoading && error && <p>{error}</p>}
-  {isLoading && <p>Loading..</p>}
-        {/*content*/}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,12 +3,25 @@ import React from 'react';
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
+
+  const deleteHandler = async () => {
+    
+    await fetch(`https://connecting-to-database-c9c11-default-rtdb.firebaseio.com/movies/${props.id}.json`, {
+      method: 'DELETE'
+    });
+
+    props.onDelete();
+  }
+
   return (
+    <React.Fragment>
     <li className={classes.movie}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <button onClick={deleteHandler}>Delete</button>
     </li>
+    </React.Fragment>
   );
 };
 
